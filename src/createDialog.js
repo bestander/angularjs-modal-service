e9477934b55ae1d38c64516b7e026cfd778cdd9e
Konstantin Raev <bestander@gmail.com>
2013-05-29 16:38:25 +1200

    changes to solve my problem. Refactor and make a pull request

--- src/createDialog.js
+++ src/createDialog.js
@@ -1,76 +1,85 @@
 angular.module('fundoo.services', []).factory('createDialog', ["$document", "$compile", "$rootScope", "$controller", "$timeout",
-      function ($document, $compile, $rootScope, $controller, $timeout) {
+    function ($document, $compile, $rootScope, $controller, $timeout) {
         var defaults = {
-          id: null,
-          title: 'Default Title',
-          backdrop: true,
-          success: {label: 'OK', fn: null},
-          controller: null, //just like route controller declaration
-          backdropClass: "modal-backdrop",
-          modalClass: "modal"
+            id: null,
+            title: 'Default Title',
+            backdrop: true,
+            success: {label: 'OK', fn: null},
+            controller: null, //just like route controller declaration
+            backdropClass: "modal-backdrop",
+            modalClass: "modal",
+            css: {
+                top: '100px',
+                left: '30%',
+                margin: '0 auto'
+            }
         };
         var body = $document.find('body');
 
         return function Dialog(template, options, passedInLocals) {
 
-          options = angular.extend({}, defaults, options); //options defined in constructor
+            options = angular.extend({}, defaults, options); //options defined in constructor
 
-          var idAttr = options.id ? ' id="' + options.id + '" ' : '';
-          //We don't have the scope we're gonna use yet, so just get a compile function for modal
-          var modalEl = angular.element(
-            '<div class="' + options.modalClass + ' fade"' + idAttr + ' ng-include="\'' + template + '\'">' +
-              '</div>');
+            var idAttr = options.id ? ' id="' + options.id + '" ' : '';
+            //We don't have the scope we're gonna use yet, so just get a compile function for modal
+            var modalEl = angular.element(
+                '<div class="' + options.modalClass + ' fade"' + idAttr + ' ng-include="\'' + template + '\'">' +
+                    '</div>');
 
-          modalEl.css('top', '100px');
-          modalEl.css('left', '30%');
-          modalEl.css('margin', '0 auto');
+            Object.keys(options.css).forEach(function (key) {
+                modalEl.css(key, options.css[key]);
+            });
 
-          var backdropEl = angular.element('<div ng-click="$modalCancel()">');
-          backdropEl.addClass(options.backdropClass);
-          backdropEl.addClass('fade in');
+            var backdropEl = angular.element('<div ng-click="$modalCancel()">');
+            backdropEl.addClass(options.backdropClass);
+            backdropEl.addClass('fade in');
 
-          var handleEscPressed = function(event) {
-            if (event.keyCode === 27) {
-              scope.$modalCancel();
+            var handleEscPressed = function(event) {
+                if (event.keyCode === 27) {
+                    scope.$modalCancel();
+                }
+            };
+
+            var closeFn = function() {
+                body.unbind('keydown', handleEscPressed);
+                modalEl.remove();
+                if (options.backdrop) {
+                    backdropEl.remove();
+                }
+            };
+
+            body.bind('keydown', handleEscPressed);
+
+            var ctrl, locals,
+                scope = options.scope || $rootScope.$new();
+
+            scope.$title = options.title;
+            scope.$modalCancel = closeFn;
+            scope.$modalSuccess = function() {
+                var callFn = options.success.fn || closeFn;
+                callFn.call(this);
+                scope.$modalCancel();
+            };
+            scope.$modalSuccessLabel = options.success.label;
+
+            if (options.controller) {
+                locals = angular.extend({$scope: scope}, passedInLocals);
+                ctrl = $controller(options.controller, locals);
+                // Yes, ngControllerController is not a typo
+                modalEl.contents().data('$ngControllerController', ctrl);
             }
-          };
 
-          var closeFn = function() {
-            body.unbind('keydown', handleEscPressed);
-            modalEl.remove();
-            if (options.backdrop) {
-              backdropEl.remove();
-            }
-          };
+            $compile(modalEl)(scope);
+            $compile(backdropEl)(scope);
+            body.append(modalEl);
+            if (options.backdrop) body.append(backdropEl);
 
-          body.bind('keydown', handleEscPressed);
 
-          var ctrl, locals,
-            scope = options.scope || $rootScope.$new();
+            $timeout(function() {
+                modalEl.addClass('in');
+            }, 0);
 
-          scope.$title = options.title;
-          scope.$modalCancel = closeFn;
-          scope.$modalSuccess = function() {
-            var callFn = options.success.fn || closeFn;
-            callFn.call(this);
-            scope.$modalCancel();
-          };
-          scope.$modalSuccessLabel = options.success.label;
+            return closeFn;
 
-          if (options.controller) {
-            locals = angular.extend({$scope: scope}, passedInLocals);
-            ctrl = $controller(options.controller, locals);
-            // Yes, ngControllerController is not a typo
-            modalEl.contents().data('$ngControllerController', ctrl);
-          }
-
-          $compile(modalEl)(scope);
-          $compile(backdropEl)(scope);
-          body.append(modalEl);
-          if (options.backdrop) body.append(backdropEl);
-
-          $timeout(function() {
-            modalEl.addClass('in');
-          }, 200);
         };
-      }]);
+    }]);
